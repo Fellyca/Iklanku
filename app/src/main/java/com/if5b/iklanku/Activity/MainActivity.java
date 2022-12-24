f22c5c4c8aa1a8c1a79c6b9f51fcaec32e26322d
YandD9 <103974529+YandD9@users.noreply.github.com>
2022-12-24 09:19:46 +0700

    1

--- app/src/main/java/com/if5b/iklanku/Activity/MainActivity.java
+++ app/src/main/java/com/if5b/iklanku/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.if5b.iklanku;
+package com.if5b.iklanku.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.if5b.iklanku.R;
 
 public class MainActivity extends AppCompatActivity {
 
