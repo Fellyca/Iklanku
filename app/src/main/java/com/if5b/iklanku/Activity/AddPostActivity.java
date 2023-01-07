e3e5d816ee9ecb24344403eaaaa83e9ba813aa5d
Fellyca <fellyca13@gmail.com>
2023-01-08 01:32:25 +0700

    upload image firebase

--- app/src/main/java/com/if5b/iklanku/Activity/AddPostActivity.java
+++ app/src/main/java/com/if5b/iklanku/Activity/AddPostActivity.java
@@ -1,22 +1,10 @@
 package com.if5b.iklanku.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.CursorLoader;
-
-import android.content.ContentUris;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.if5b.iklanku.API.APIServices;
@@ -31,11 +19,6 @@ import retrofit2.Response;
 public class AddPostActivity extends AppCompatActivity {
     private ActivityAddPostBinding binding;
 
-    private int PICK_IMAGE_REQUEST = 1;
-    private Uri filepath;
-    private Bitmap bitmap;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,21 +27,10 @@ public class AddPostActivity extends AppCompatActivity {
 
         binding.progressBar.setVisibility(View.GONE);
 
-        binding.btnSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ShowFileChooser();
-            }
-        });
-
-        binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+        binding.btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String judul = binding.etJudul.getText().toString();
-                String tv4 = binding.tv4.getText().toString();
-                Context context = AddPostActivity.this;
-                String path = getPath(context, filepath);
-
                 boolean bolehPost = true;
 
                 if(TextUtils.isEmpty(judul)){
@@ -66,14 +38,13 @@ public class AddPostActivity extends AppCompatActivity {
                     binding.etJudul.setError("judul tidak boleh kosong!");
                 }
 
-                if(TextUtils.isEmpty(tv4)){
-                    bolehPost = false;
-                    binding.etJudul.setError("gambar tidak boleh kosong!");
+                if (bolehPost){
+                    addPost(Utilities.getValue(AddPostActivity.this, "xUsername"), judul, "");
                 }
 
-                if (bolehPost){
-                    addPost(Utilities.getValue(AddPostActivity.this, "xUsername"), judul, path);
-                }
+                Intent intent = new Intent(AddPostActivity.this, UploadImageActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
@@ -83,8 +54,6 @@ public class AddPostActivity extends AppCompatActivity {
         APIServices api = Utilities.getRetrofit().create(APIServices.class);
         Call<ValueNoData> call = api.insertPost(Utilities.API_KEY, username, judul, image);
         call.enqueue(new Callback<ValueNoData>() {
-            //untuk 2 baris diatas
-            //api.insertPost(Utilities.API_KEY, username, content).enqueue(new Callback<ValueNoData>(){
             @Override
             public void onResponse(Call<ValueNoData> call, Response<ValueNoData> response) {
                 if (response.code() == 200){
@@ -111,182 +80,4 @@ public class AddPostActivity extends AppCompatActivity {
             }
         });
     }
-    private void ShowFileChooser() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_IMAGE_REQUEST && data != null && data.getData() != null) {
-
-            filepath = data.getData();
-            Context context = AddPostActivity.this;
-            try {
-
-                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
-                binding.ivImage.setImageBitmap(bitmap);
-                binding.tv4.setText(filepath.toString());
-                Toast.makeText(getApplicationContext(),getPath(context, filepath),Toast.LENGTH_LONG).show();
-            } catch (Exception ex) {
-
-            }
-        }
-    }
-
-    private String getPath(Context context, Uri uri) {
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-
-        // DocumentProvider
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            // ExternalStorageProvider
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-
-            }
-            // DownloadsProvider
-            else if (isDownloadsDocument(uri)) {
-
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[]{
-                        split[1]
-                };
-
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-            }
-        }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
-
-            // Return the remote address
-            if (isGooglePhotosUri(uri))
-                return uri.getLastPathSegment();
-
-            return getDataColumn(context, uri, null, null);
-        }
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
-        }
-
-        return null;
-
-
-
-//        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-//        cursor.moveToFirst();
-//        String document_id = cursor.getString(0);
-//        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
-//        cursor.close();
-//
-//        cursor = getContentResolver().query(
-//                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-//                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
-//        cursor.moveToFirst();
-//        String path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
-//        cursor.close();
-//        return path;
-//
-//        String[] proj = {MediaStore.Images.Media.DATA};
-//        String result = null;
-//
-//        CursorLoader cursorLoader = new CursorLoader(context, uri, null, null, null, null);
-//        Cursor cursor = cursorLoader.loadInBackground();
-//
-//        if (cursor != null) {
-//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//            cursor.moveToFirst();
-//            result = cursor.getString(column_index);
-//            cursor.close();
-//        }
-//        return result;
-    }
-
-    public static String getDataColumn(Context context, Uri uri, String selection,
-                                       String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {
-                column
-        };
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
-        }
-        return null;
-    }
-
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is ExternalStorageProvider.
-     */
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is Google Photos.
-     */
-    public static boolean isGooglePhotosUri(Uri uri) {
-        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
-    }
-
 }
---
+++ app/src/main/java/com/if5b/iklanku/Activity/UploadImageActivity.java
@@ -0,0 +1,99 @@
+package com.if5b.iklanku.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.if5b.iklanku.databinding.ActivityUploadImageBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class UploadImageActivity extends AppCompatActivity {
+    private ActivityUploadImageBinding binding;
+    Uri imageUri;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUploadImageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        binding.btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+    }
+
+    private void uploadImage() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Upload File...");
+        progressDialog.show();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+        Date now = new Date();
+        String fileName = formatter.format(now);
+        storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        binding.ivImage.setImageURI(null);
+                        Toast.makeText(UploadImageActivity.this, "Upload Berhasil", Toast.LENGTH_SHORT).show();
+                        if(progressDialog.isShowing())
+                            progressDialog.dismiss();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+
+                        Toast.makeText(UploadImageActivity.this, "Upload gagal", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
+    private void selectImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100 && data!= null && data.getData()!= null){
+            imageUri = data.getData();
+            binding.ivImage.setImageURI(imageUri);
+        }
+    }
+}
